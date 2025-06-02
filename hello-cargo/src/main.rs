5af0c5a7fabc2d3fabcfca62f2f5067836a0d691
Wasif Islam <islamwasif3@gmail.com>
2025-06-02 00:38:15 -0400

    chore: adding taskfile to project

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,10 +1,33 @@
+#![deny(
+    bad_style,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    unconditional_recursion,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true
+)]
+
 use std::io;
 
 fn main() {
-
     // Going to write a guessing game
     println!("Welcome to My Guessing Game!");
 
+    println!("Please input your guess: ");
 
-    println!("Hello, world!");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
